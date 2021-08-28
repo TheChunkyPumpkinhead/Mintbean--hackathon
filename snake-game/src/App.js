00b4b1f5ae0421aae3e1aa21afcc6eb0018c90b6
jesus_garcia <voli.scorp13@gmail.com>
2021-08-27 20:36:32 -0500

    other sec

--- snake-game/src/App.js
+++ snake-game/src/App.js
@@ -5,7 +5,7 @@ import MainContainer from "./container/MainContainer";
 
 function App() {
   return (
-    <div>
+    <div className='app-container'>
       <Header />
       <MainContainer />
       <footer>Snake game created by Jesus and Chris</footer>
